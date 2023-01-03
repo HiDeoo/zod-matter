4067356ddcaa709ba524e61a9df675e2f075578a
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-01-03 17:11:03 +0100

    feat: add basic parsing

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import matter from 'gray-matter'
+import { type z, type AnyZodObject } from 'zod'
+
+export function parse<TSchema extends AnyZodObject, TInput extends matter.Input>(
+  input: TInput,
+  schema: TSchema
+): ZodMatterFile<TSchema, TInput> {
+  const { data, ...dataLessFile } = matter(input)
+
+  return {
+    ...dataLessFile,
+    data: schema.parse(data),
+  }
+}
+
+type ZodMatterFile<TSchema extends AnyZodObject, TInput extends matter.Input> = Omit<
+  matter.GrayMatterFile<TInput>,
+  'data'
+> & {
+  data: z.infer<TSchema>
+}
